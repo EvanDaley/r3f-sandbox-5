94158632a99a50aa8c6a7deda5e24d8426538d01
Evan <daley.evan@gmail.com>
2025-10-11 15:02:15 -0700

    Adding a toon shader

--- src/modules/dynamic_colors/TileLevel1.js
+++ src/modules/dynamic_colors/TileLevel1.js
@@ -1,18 +1,18 @@
 ﻿import OrthoV2 from "../controls/OrthoV2";
 import SimpleLighting from "../environment/SimpleLighting";
-import {MeshTransmissionMaterial} from "@react-three/drei";
 
 export default function TileLevel1() {
     console.log('here')
     return (
         <>
             <color attach="background" args={['#181818']} />
-
             <OrthoV2/>
-            <SimpleLighting/>
+            <SimpleLighting
+                directionalIntensity={3}
+            />
             <mesh>
-                <boxGeometry args={[1, 1, 1]} />
-                < color="green" />
+                <sphereGeometry args={[1, 10, 10]} />
+                <meshToonMaterial  color="green" />
             </mesh>
         </>
     );
