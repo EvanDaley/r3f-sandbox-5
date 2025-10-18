25e0f793547191bfc5a3b5786352390350dd6351
Evan <daley.evan@gmail.com>
2025-10-18 10:15:45 -0700

    Cleaning up project structure

--- src/modules/networked_chat/NetworkedChat.js
+++
@@ -1,7 +0,0 @@
-﻿export default function NetworkedChat() {
-  return (
-    <>
-      hello
-    </>
-  )
-}
--- src/modules/networked_chat/NetworkedChatOverlay.js
+++
@@ -1,7 +0,0 @@
-﻿export default function NetworkedChatOverlay() {
-  return (
-    <>
-      hello
-    </>
-  )
-}
