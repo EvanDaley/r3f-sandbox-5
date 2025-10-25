99af682d2546db9366ebb750beef261ac82cc4f4
Evan <daley.evan@gmail.com>
2025-10-25 13:06:28 -0700

    Adding some obstacles in the movement sandbox

--- src/modules/networking_focus/movement_sandbox_2/MovementSandbox2.js
+++ src/modules/networking_focus/movement_sandbox_2/MovementSandbox2.js
@@ -1,7 +1,7 @@
-﻿import React, { useRef } from "react";
-import { useRobotMovement } from "./hooks/useRobotMovement";
-import { usePlayerStore } from "./stores/usePlayerStore";
-import { usePeerStore } from "../general_connection_tooling/stores/peerStore";
+﻿import React, {useRef} from "react";
+import {useRobotMovement} from "./hooks/useRobotMovement";
+import {usePlayerStore} from "./stores/usePlayerStore";
+import {usePeerStore} from "../general_connection_tooling/stores/peerStore";
 import OrthoZoomOnly from "../../../components/controls/OrthoZoomOnly";
 import SimpleLighting2 from "../../../components/environment/SimpleLighting2";
 import EffectsV2 from "../../../components/effects/EffectsV2";
@@ -10,13 +10,22 @@ import FloatingRobot from "../../dynamic_colors/objects/FloatingRobot";
 import {usePaletteStore} from "../../dynamic_colors/stores/paletteStore";
 import LittleRobot from "../../dynamic_colors/objects/LittleRobot";
 import {useInitPlayer} from "./hooks/useInitPlayer";
+import Building1 from "../../dynamic_colors/objects/Building1";
+import Building2 from "../../dynamic_colors/objects/Building2";
+import Tree1 from "../../dynamic_colors/objects/Tree1";
+import Turret1 from "../../dynamic_colors/objects/Turret1";
+import Turret2 from "../../dynamic_colors/objects/Turret2";
+import Turret3 from "../../dynamic_colors/objects/Turret3";
 
 export default function MovementSandbox2() {
   useInitPlayer();
 
   const activePalette = usePaletteStore((s) => s.activePalette);
   const players = usePlayerStore((s) => s.players);
-  const { peerId, hostId } = usePeerStore((s) => ({
+  const {
+    peerId,
+    hostId
+  } = usePeerStore((s) => ({
     peerId: s.peerId,
     hostId: s.hostId,
   }));
@@ -59,11 +68,48 @@ export default function MovementSandbox2() {
       })}
 
 
-      <color attach="background" args={["#3c2828"]} />
-      <OrthoZoomOnly />
-      <SimpleLighting2 />
-      <EffectsV2 />
-      <TileGrid />
+      <color attach="background" args={["#3c2828"]}/>
+      <OrthoZoomOnly/>
+      <SimpleLighting2/>
+      <EffectsV2/>
+      <TileGrid/>
+
+      <group position={[-3, 0, 0]}>
+
+        <group position={[0, 0, -3]} >
+          <Building1
+            materials={activePalette}
+          />
+
+          <Building2
+            position={[-7.5, 0, 2]}
+            materials={activePalette}
+          />
+        </group>
+
+        <group position={[-3, 0, 0]} rotation={[0,Math.PI/2, 0]}>
+          <Turret1
+            materials={activePalette}
+            position={[-3, 0, 0]}
+          />
+          <Turret2
+            materials={activePalette}
+            position={[-6, 0, 0]}
+          />
+          <Turret3
+            materials={activePalette}
+            position={[0, 0, 0]}
+          />
+
+        </group>
+        <Tree1
+          materials={activePalette}
+          position={[-8, 0, 6]}
+          scale={[1.5, 1.5, 1.5]}
+        />
+
+
+      </group>
     </>
   );
 }
