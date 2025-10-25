d7089a26c7c66f5d722884025029e32dbfeeb678
Evan <daley.evan@gmail.com>
2025-10-25 10:38:20 -0700

    Adding a generic broadcastMessage helper

--- src/modules/networking_focus/general_connection_tooling/broadcastSceneChange.js
+++ src/modules/networking_focus/general_connection_tooling/broadcastSceneChange.js
@@ -1,24 +1,12 @@
-﻿import {usePeerStore} from "./stores/peerStore";
+﻿import { broadcastMessage } from "./broadcastMessage";
 
 export const broadcastSceneChange = (sceneId) => {
-  const { connections, isHost } = usePeerStore.getState();
-
-  if (!isHost) {
-    console.warn('Only hosts can broadcast scene changes');
-    return;
-  }
-
-  const message = {
-    scene: 'common',
-    type: 'changeScene',
-    payload: { sceneId }
-  };
-
-  // Send to all connected clients
-  Object.values(connections).forEach(({ conn }) => {
-    if (conn && conn.open) {
-      conn.send(message);
-      console.log(`Sent scene change to ${conn.peer}: ${sceneId}`);
-    }
-  });
+  broadcastMessage(
+    {
+      scene: "common",
+      type: "changeScene",
+      payload: { sceneId },
+    },
+    { hostOnly: true }
+  );
 };
