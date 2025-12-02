965bb752a2506c5eadf8ad7ff73e7046ed56d366
Evan <daley.evan@gmail.com>
2025-12-01 18:29:36 -0800

    Pushing another build

--- src/modules/item_configurator_1/ItemConfigurator1.js
+++ src/modules/item_configurator_1/ItemConfigurator1.js
@@ -11,9 +11,8 @@ export const state = proxy({
 export default function ItemConfigurator1() {
   return (
     <>
-      <color attach="background" args={["#ffffff"]} />
       {/* <perspectiveCamera makeDefault position={[0, 0, 4]} fov={45} /> */}
-      <perspectiveCamera makeDefault  />
+      <perspectiveCamera makeDefault } />
       <ambientLight intensity={0.7} />
       <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
       <Shoe />
